01e5b935c098bf14cb71aef9e3f9eb5e6f3fded7
Francis7799 <146633009+Francis7799@users.noreply.github.com>
2026-08-08 02:01:12 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,8 +2,6 @@ function calculateTax(amount) {
     return amount * 0.18;
 }
 
-function formatPrice(amount) {
-    return '$' + amount.toFixed(2);
-}
+
 
 module.exports = { calculateTax, formatPrice };

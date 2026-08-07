33b055b3678d3f25f5d823e778a776b0d681a831
Francis7799 <146633009+Francis7799@users.noreply.github.com>
2026-08-08 02:03:29 +0530

    Remove formatPrice function from utils.js
    Removed the formatPrice function from utils.js.

--- utils.js
+++ utils.js
@@ -2,9 +2,5 @@ function calculateTax(amount) {
     return amount * 0.18;
 }
 
-function formatPrice(amount) {
-    return '$' + amount.toFixed(2);
-}
-
 
 module.exports = { calculateTax, formatPrice };

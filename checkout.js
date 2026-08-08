46e61052ae51ce30565e35ce4aff01ba7a0ae3b6
Francis7799 <146633009+Francis7799@users.noreply.github.com>
2026-08-08 08:49:48 +0530

    Fix typo in loyalty points processing function

--- checkout.js
+++ checkout.js
@@ -2,6 +2,6 @@ function processCheckout(cart, user) {
     const tax = calculateTax(cart.total);         
     const total = formatPrice(cart.total + tax); 
 
-    const points = processLoyaltyPoints(user);     
+    const points = processLoyaltyPoint(user);     
     return { total, points };
 }
